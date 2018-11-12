a6dcd9b9189e2957c424cb4d3ddc5bdf6fe42730
aaronmguiang <43330157+aaronmguiang@users.noreply.github.com>
2018-11-13 00:31:58 +0800

    Initial commit

--- app/src/main/java/com/kyle/vallidofinalproject/MainActivity.java
+++ app/src/main/java/com/kyle/vallidofinalproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kyle.vallidofinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void toExercise(View v) {
+
+        Intent i = new Intent(packageContext: this, exercise.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/kyle/vallidofinalproject/exercise.java
@@ -0,0 +1,4 @@
+package com.kyle.vallidofinalproject;
+
+public class exercise {
+}
